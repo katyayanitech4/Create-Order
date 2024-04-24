cff5522fc5669a18da4313cca33ee9d8f1f88bbb
katyayanitech2 <katyayanitech2@gmail.com>
2024-04-24 12:36:13 +0530

    Port update

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const easyecominvoice = require("./routes/createorder.js");
 
 const app = express();
 app.use("/createorder", easyecominvoice);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+}); 
