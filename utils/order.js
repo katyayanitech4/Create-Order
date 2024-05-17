e773c7b6d8c372adefe6394d4dcf524ed6093878
katyayanitech4 <164606206+katyayanitech4@users.noreply.github.com>
2024-05-17 16:57:41 +0530

    log edit cutome feild

--- utils/order.js
+++ utils/order.js
@@ -423,7 +423,7 @@ exports.postordercreate = async (invoice) => {
     console.log(`Custome Feild ${invoice[0].order_items.custom_fields}`);
     console.log("Salesperson Id ", invoice[0].reference_code.split("/")[1]);
     console.log("easyecom order history", invoice[0].easyecom_order_history);
-    console.log(`Custome Feild ${invoice[0].order_items.custom_fields}`);
+    console.log(`Custome Feild ${invoice[0].order_items[0].custom_fields}`);
     console.log(`Custome Feild ${invoice[0].order_items.custom_fields[1]}`);
 
     try {
